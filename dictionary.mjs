92ca5f6e9187676b408f8ce5f96ea85c1e2c92e1
idaspa <idahelenhal@msn.com>
2025-03-19 14:36:58 +0100

    dictionary list

---
+++ dictionary.mjs
@@ -0,0 +1,11 @@
+const elements = [
+
+    { "AlchemySymbol": "Au", "PlanetSymbol": "☉", "Metal": "Gold", "Name": "Aurum", "AtomicNumber": 79 },
+    { "AlchemySymbol": "Hg", "PlanetSymbol": "☿", "Metal": "Quicksilver", "Name": "Mercury", "AtomicNumber": 80 },
+    { "AlchemySymbol": "Ag", "PlanetSymbol": "☽", "Metal": "Silver", "Name": "Argentum", "AtomicNumber": 47 },
+    { "AlchemySymbol": "Fe", "PlanetSymbol": "♂", "Metal": "Iron", "Name": "Ferrum", "AtomicNumber": 26 },
+    { "AlchemySymbol": "Au", "PlanetSymbol": "☉", "Metal": "Gold", "Name": "Aurum", "AtomicNumber": 79 },
+];
+
+
+export default elements;
