ff0cd7014a1fbfdd9b66569a3fb3432910b9b144
idaspa <idahelenhal@msn.com>
2025-03-24 20:45:04 +0100

    New List of symbol and names

--- listOfStrings.mjs
+++ listOfStrings.mjs
@@ -5,7 +5,7 @@ const elements = [
     { "AlchemySymbol": "Ag", "PlanetSymbol": "☽", "Metal": "Silver", "Name": "Argentum", "AtomicNumber": 47 },
     { "AlchemySymbol": "Fe", "PlanetSymbol": "♂", "Metal": "Iron", "Name": "Ferrum", "AtomicNumber": 26 },
     { "AlchemySymbol": "Au", "PlanetSymbol": "☉", "Metal": "Gold", "Name": "Aurum", "AtomicNumber": 79 },
-    { "AlchemySymbol": "d", "PlanetSymbol": "♀", "Met": "Copper", "Name": "d", "AtomicNumber": "d" },
+    
     
 ];
 
---
+++ listOfSymbol.mjs
@@ -0,0 +1,14 @@
+const symbolsOfElements = [
+   
+    { "Symbol": "☿", "Name": "Mercury" },
+    { "Symbol": "♀", "Name": "Copper" },
+    { "Symbol": "🜍", "Name": "sulfur" },
+    { "Symbol": "🜔", "Name": "salt" },
+    { "Symbol": "🜄", "Name": "Water" },
+    { "Symbol": "🜂", "Name": "Fire" }, 
+    { "Symbol": "☉", "Name": "Gold" },
+    { "Symbol": "🜁", "Name": "Air" },
+
+
+];
+export default symbolsOfElements
