ec40c22fa93c9bd42dd133d6cb61ab6c82a5d1d4
idaspa <idahelenhal@msn.com>
2025-03-24 20:43:04 +0100

    cleaner

---
+++ listOfStrings.mjs
@@ -0,0 +1,14 @@
+const elements = [
+
+    { "AlchemySymbol": "Au", "PlanetSymbol": "☉", "Metal": "Gold", "Name": "Aurum", "AtomicNumber": 79 },
+    { "AlchemySymbol": "Hg", "PlanetSymbol": "☿", "Metal": "Quicksilver", "Name": "Mercury", "AtomicNumber": 80 },
+    { "AlchemySymbol": "Ag", "PlanetSymbol": "☽", "Metal": "Silver", "Name": "Argentum", "AtomicNumber": 47 },
+    { "AlchemySymbol": "Fe", "PlanetSymbol": "♂", "Metal": "Iron", "Name": "Ferrum", "AtomicNumber": 26 },
+    { "AlchemySymbol": "Au", "PlanetSymbol": "☉", "Metal": "Gold", "Name": "Aurum", "AtomicNumber": 79 },
+    { "AlchemySymbol": "d", "PlanetSymbol": "♀", "Met": "Copper", "Name": "d", "AtomicNumber": "d" },
+    
+];
+
+
+
+export default elements;
